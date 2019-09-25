9da66d8aeccf6f5bfb003f41d8db77179c231605
gintare urbanaviciute <gintare.urban@gmail.com>
2019-09-25 18:14:07 +0300

    HelloWorld classimplemented

---
+++ src/lt/vtmc/pd01/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package lt.vtmc.pd01;
+
+/**
+ * The Hello World class implements an application
+ * that prints "Hello World!" to standard output
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Prints "Hello World!" to the terminal window.
+		System.out.println("Hello World!");
+	}
+
+}
